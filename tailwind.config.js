2c3afffc8f7113f43603811216a8e7abdd5ed748
Nathan Gillespie <38772830+speedmonster45@users.noreply.github.com>
2024-09-22 17:59:44 -0400

    commit for aws based static webpage with tailwind optimized for zombies

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './*.html',               // Watch all HTML files in the root directory
+    './assets/js/**/*.js',     // Watch all JS files in the assets/js directory
+    './assets/css/**/*.css',   // Watch all CSS files if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

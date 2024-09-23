cff434e2d65db629ebcf92e6cb35e3dbb7d12e28
Nathan Gillespie <38772830+speedmonster45@users.noreply.github.com>
2024-09-22 22:20:37 -0400

    commiting changes for jekyll

--- assets/js/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Page is loaded and ready!");
-    
-    const header = document.querySelector('header');
-    
-    header.addEventListener('click', function() {
-        alert("Header clicked!");
-    });
-});
